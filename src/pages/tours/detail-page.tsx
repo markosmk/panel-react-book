2c599819c181f2296b3c28b912345266cc3dc96f
Markos Mk <markosmk@me.com>
2024-12-14 11:46:40 -0300

    add: last update in header

--- src/pages/tours/detail-page.tsx
+++ src/pages/tours/detail-page.tsx
@@ -1,29 +1,34 @@
-import { useTourDetail } from '@/services/hooks/tour.query';
 import { useParams } from 'react-router-dom';
+
 import { FormTour } from './form-tour';
+import { useTourDetail } from '@/services/hooks/tour.query';
+import { cn, formatDateOnly } from '@/lib/utils';
+
 import { Card, CardContent } from '@/components/ui/card';
 import { PendingContent } from '@/components/pending-content';
 import { HeadingMain } from '@/components/heading-main';
-import { cn } from '@/lib/utils';
 
 export default function TourDetailPage() {
   const { tourId } = useParams();
   const {
     data: tourDetail,
     isLoading,
-    isFetching
+    isFetching,
+    isError
   } = useTourDetail(tourId || '');
 
   if (isLoading) return <PendingContent />;
 
   // TODO: better error handling
-  if (!tourDetail) return <>No hay tour</>;
+  if (!tourDetail) return <></>;
+
+  if (isError) return <div>Error</div>;
 
   return (
     <div className="mx-auto w-full max-w-2xl space-y-8 px-6">
       <HeadingMain
         title={tourDetail.tour.name}
-        description="Edita los datos de tu tour."
+        description={`Ultima actualizacion: ${formatDateOnly(tourDetail.tour.updated_at)}`}
       />
       <Card className={cn(isFetching && 'cursor-wait')}>
         <CardContent>
