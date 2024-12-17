4dfc4f2d573dc37591f134b56da1c87e5e06f301
Markos Mk <markosmk@me.com>
2024-12-17 00:50:05 -0300

    add: correct api in constants

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,8 @@
 import { Config } from '@/types/app.types';
 
 export const CONFIG: Config = {
-  apiUrl: 'https://zorzal.host/api/public/index.php/api/v1',
+  apiUrl: 'https://api.zorzalwines.com/public/index.php/api/v1',
+  // apiUrl: 'https://zorzal.host/api/public/index.php/api/v1',
   // apiUrl: 'http://localhost:8080/api/v1',
   defaultTheme: 'system',
   storageKey: 'vite-ui-theme',
