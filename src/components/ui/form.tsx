6349889599c74a50c3c0e600ed8b51a175af9338
Markos Mk <markosmk@me.com>
2024-12-31 21:20:25 -0300

    feat: add required indicator to form label component

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -86,8 +86,10 @@ FormItem.displayName = 'FormItem';
 
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & {
+    required?: boolean;
+  }
+>(({ className, required, ...props }, ref) => {
   const { error, formItemId } = useFormField();
 
   return (
@@ -96,7 +98,33 @@ const FormLabel = React.forwardRef<
       className={cn(error && 'text-destructive', className)}
       htmlFor={formItemId}
       {...props}
-    />
+    >
+      <span className="flex items-center">
+        {props.children}
+        {required && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="ml-1 inline-flex size-3 text-destructive"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12l8 -4.5" />
+            <path d="M12 12v9" />
+            <path d="M12 12l-8 -4.5" />
+            <path d="M12 12l8 4.5" />
+            <path d="M12 3v9" />
+            <path d="M12 12l-8 4.5" />
+          </svg>
+        )}
+      </span>
+    </Label>
   );
 });
 FormLabel.displayName = 'FormLabel';
