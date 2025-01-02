e78d8dc48c9e2296c99a0317e7ba5a89aa3fad48
Markos Mk <markosmk@me.com>
2025-01-02 17:52:27 -0300

    feat: add toast notification for 429 errors in axios interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,8 @@
+import axios from 'axios';
+import { toast } from 'sonner';
+
 import { CONFIG } from '@/constants/config';
 import { queryClient } from '@/providers';
-import axios from 'axios';
 
 const axiosApp = axios.create({
   baseURL: CONFIG.apiUrl,
@@ -8,40 +10,29 @@ const axiosApp = axios.create({
 });
 
 axiosApp.interceptors.request.use((config) => {
-  // const token = localStorage.getItem(CONFIG.cookieName);
-  // if (token) {
-  //   config.headers.Authorization = `Bearer ${token}`;
-  // }
-  // const tokenUser = Cookies.get(CONFIG.cookieName);
-  // const token = Cookies.get('app_token');
-  // console.log('tokenuser', tokenUser);
-  // console.log('token', token);
-
   return config;
 });
 
 axiosApp.interceptors.response.use(
   (response) => {
-    // checkear si existe token en cookies
-    // const tokenUser = Cookies.get(CONFIG.cookieName);
-    // const token = Cookies.get('app_token');
-    // console.log('tokenuser', tokenUser);
-    // console.log('token', token);
     return response;
   },
   (error) => {
-    console.log(error);
-
-    // const tokenUser = Cookies.get(CONFIG.cookieName);
-    // const token = Cookies.get('app_token');
-    // console.log('tokenuser', tokenUser);
-    // console.log('token', token);
-
     if (error.response?.status === 401) {
       localStorage.removeItem('accessToken');
-      queryClient.invalidateQueries({ queryKey: ['currentUser'] });
-
+      queryClient.clear();
+      // queryClient.invalidateQueries({ queryKey: ['currentUser'] });
       window.location.href = '/login';
+    } else if (
+      error.response?.status === 429 &&
+      error.config.method === 'post'
+    ) {
+      toast.warning(
+        'Demasiados intentos, intenta de nuevo más tarde o contacta al soporte.',
+        {
+          duration: 5000
+        }
+      );
     }
     return Promise.reject(error);
   }
