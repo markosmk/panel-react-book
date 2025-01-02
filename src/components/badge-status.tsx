4c3def73c82c9f0f126eef24eb273036f5659d8a
Markos Mk <markosmk@me.com>
2025-01-01 21:51:52 -0300

    feat: add tooltip helper and icon updates in BadgeStatus component

--- src/components/badge-status.tsx
+++ src/components/badge-status.tsx
@@ -1,48 +1,76 @@
-import { cn } from '@/lib/utils';
+/* eslint-disable react-refresh/only-export-components */
+import { Status } from '@/types/booking.types';
 
 import { IconSVG, Icons } from './icons';
 import { Badge, BadgeProps } from './ui/badge';
-import { Status } from '@/types/booking.types';
+import { TooltipHelper } from './tooltip-helper';
+import { cn } from '@/lib/utils';
 
 type BadgeStatusProps = BadgeProps & {
   status: Status | 'Default';
-  useIcons?: boolean;
+  useIcon?: boolean;
 };
 
-const currentVariant: Record<
-  string,
+type StatusVariant = Record<
+  Status | 'Default',
   {
     variant: 'wrong' | 'success' | 'warning' | 'default';
     icon: IconSVG;
     text: string;
+    className?: string;
   }
-> = {
-  CANCELED: { variant: 'wrong', icon: Icons.warning, text: 'Cancelado' },
-  CONFIRMED: { variant: 'success', icon: Icons.success, text: 'Confirmado' },
-  PENDING: { variant: 'warning', icon: Icons.pending, text: 'Pendiente' },
-  Default: { variant: 'default', icon: Icons.help, text: 'N/A' }
+>;
+
+export const statusVariant: StatusVariant = {
+  CONFIRMED: {
+    variant: 'success',
+    icon: Icons.success,
+    text: 'Confirmado',
+    className: 'text-green-600'
+  },
+  PENDING: {
+    variant: 'warning',
+    icon: Icons.pending,
+    text: 'Pendiente',
+    className: 'text-yellow-600'
+  },
+  CANCELED: {
+    variant: 'wrong',
+    icon: Icons.warning,
+    text: 'Cancelado',
+    className: 'text-red-600'
+  },
+  Default: {
+    variant: 'default',
+    icon: Icons.help,
+    text: 'N/A',
+    className: 'text-gray-600'
+  }
 };
 
 export function BadgeStatus({
   status = 'Default',
-  useIcons = false,
+  useIcon = false,
   ...props
 }: BadgeStatusProps) {
   const {
     variant,
     icon: Icon,
-    text
-  } = currentVariant[status] ?? currentVariant['Default'];
+    text,
+    className
+  } = statusVariant[status] ?? statusVariant['Default'];
+
+  if (useIcon) {
+    return (
+      <TooltipHelper content={text}>
+        <Icon className={cn('h-4 w-4', className)} />
+      </TooltipHelper>
+    );
+  }
+
   return (
-    <Badge
-      variant={variant}
-      className={cn(
-        useIcons && 'flex h-8 w-8 items-center justify-center p-0',
-        props.className
-      )}
-      {...props}
-    >
-      {useIcons ? <Icon className="h-4 w-4" /> : text}
+    <Badge variant={variant} className={props.className} {...props}>
+      {text}
     </Badge>
   );
 }
