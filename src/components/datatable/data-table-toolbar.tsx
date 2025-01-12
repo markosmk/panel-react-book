f588d7699c192e51e63cf12f0162d037abc0b2c7
Markos Mk <markosmk@me.com>
2025-01-12 10:57:28 -0300

    feat: implement enhanced global search functionality in data table

--- src/components/datatable/data-table-toolbar.tsx
+++ src/components/datatable/data-table-toolbar.tsx
@@ -4,34 +4,27 @@ import { Input } from '@/components/ui/input';
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
-  columnToSearch: string | number | symbol;
-  columnName: string;
+  placeholderSearch?: string;
   actions?: React.ReactNode;
+  globalFilter: string;
+  setGlobalFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export function DataTableToolbar<TData>({
   table,
-  columnToSearch,
-  columnName,
+  placeholderSearch = 'Buscar por...',
+  globalFilter,
+  setGlobalFilter,
   actions
 }: DataTableToolbarProps<TData>) {
   return (
     <div className="flex items-center pb-4">
       <Input
-        placeholder={`Buscar por ${columnName}...`}
-        value={
-          (table
-            .getColumn(columnToSearch as string)
-            ?.getFilterValue() as string) ?? ''
-        }
-        onChange={(event) =>
-          table
-            .getColumn(columnToSearch as string)
-            ?.setFilterValue(event.target.value)
-        }
-        // value={table.getState()?.globalFilter || ""}
-        // onChange={(event) => table.setGlobalFilter(String(event.target.value))}
-        className="max-w-sm"
+        placeholder={placeholderSearch}
+        value={globalFilter}
+        disabled={table.getFilteredSelectedRowModel().rows.length > 0}
+        onChange={(e) => setGlobalFilter(e.target.value)}
+        className="w-full sm:max-w-lg"
       />
       <div className="ml-auto">{actions && actions}</div>
     </div>
--- src/components/datatable/index.tsx
+++ src/components/datatable/index.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { Cross2Icon } from '@radix-ui/react-icons';
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -8,9 +10,7 @@ import {
   getSortedRowModel,
   // SortingState,
   useReactTable
-  // VisibilityState,
 } from '@tanstack/react-table';
-import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -21,67 +21,37 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table';
-
-import { DataTableToolbar } from './data-table-toolbar';
-import { useSearchParams } from 'react-router-dom';
+import { Input } from '@/components/ui/input';
+import { Icons } from '@/components/icons';
+import { MenuSelection } from './menu-selection';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
-  columnToSearch: string | number | symbol;
-  columnName?: string;
+  columnsToSearch?: (keyof TData)[];
+  placeholderSearch?: string;
   children?: React.ReactNode;
-  range?: { from: Date; to: Date };
-  pageCount?: number;
+  handleDeleteSelected?: (data: TData[]) => Promise<boolean>;
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
-  columnToSearch,
-  columnName = 'item',
-  children,
-  range,
-  pageCount = 0
+  columnsToSearch,
+  placeholderSearch,
+  handleDeleteSelected,
+  children
 }: DataTableProps<TData, TValue>) {
   // const [sorting, setSorting] = React.useState<SortingState>([])
+  // const [rowSelection, setRowSelection] = React.useState({})
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
-  // const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-  // const [rowSelection, setRowSelection] = React.useState({})
-  // const [globalFilter, setGlobalFilter] = React.useState<string>("")
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  // Search params
-  const page = searchParams?.get('page') ?? '1';
-  const pageAsNumber = Number(page);
-  const fallbackPage =
-    isNaN(pageAsNumber) || pageAsNumber < 1 ? 1 : pageAsNumber;
-  const per_page = searchParams?.get('limit') ?? '10';
-  const perPageAsNumber = Number(per_page);
-  const fallbackPerPage = isNaN(perPageAsNumber) ? 10 : perPageAsNumber;
-
-  // Handle server-side pagination
-  const [{ pageIndex, pageSize }, setPagination] = React.useState({
-    pageIndex: fallbackPage - 1,
-    pageSize: fallbackPerPage
-  });
-
-  React.useEffect(() => {
-    // Update the URL with the new page number and limit
-    setSearchParams({
-      ...Object.fromEntries(searchParams), // Spread the existing search params
-      page: (pageIndex + 1).toString(), // Update the page number (assuming pageIndex is 0-based)
-      limit: pageSize.toString() // Update the limit
-    });
-    // if search is there setting filter value
-  }, [pageIndex, pageSize, searchParams, setSearchParams]);
+  const [globalFilter, setGlobalFilter] = React.useState<string>('');
 
   const table = useReactTable({
     data,
     columns,
-    pageCount: pageCount ?? -1,
     // onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
@@ -89,39 +59,79 @@ export function DataTable<TData, TValue>({
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     // onRowSelectionChange: setRowSelection,
-    // TODO: globalFilter add, to search more than one word
-    // onGlobalFilterChange: setGlobalFilter,
-    // globalFilterFn: (row, columnIds, filterValue) => {
-    //   const searchWords: string[] = filterValue.toLowerCase().split(" ")
-    //   if ([columnName].includes(columnIds as string)) {
-    //     const columnName: keyof TData = columnIds as keyof TData
-    //     const cellValue = row.original[columnName]?.toString().toLowerCase()
-    //     return searchWords.every((word) => cellValue?.includes(word))
-    //   }
-    //   return false
-    // },
     state: {
       // sorting,
-      columnFilters,
       // rowSelection,
-      // globalFilter,
-      pagination: { pageIndex, pageSize }
+      columnFilters,
+      globalFilter
     },
-    onPaginationChange: setPagination,
     getPaginationRowModel: getPaginationRowModel(),
-    manualPagination: true,
-    manualFiltering: true
+    globalFilterFn: (row, columnId, filterValue): boolean => {
+      if (!columnsToSearch) return false;
+      // must be exist data in column
+      // if (!columnsToCheck.includes(columnId as keyof CustomerTable)) {
+      //   return false;
+      // }
+
+      // const cellValue = row.original[columnId];
+      // return (
+      //   cellValue &&
+      //   cellValue.toString().toLowerCase().includes(filterValue.toLowerCase())
+      // );
+      const lowerCaseFilterValue = filterValue.toLowerCase();
+      const searchTerms = lowerCaseFilterValue.split(' ').filter(Boolean);
+
+      return columnsToSearch.some((column) => {
+        const cellValue = row.original[column as keyof TData];
+        if (!cellValue) return false;
+
+        const cellValueString = cellValue.toString().toLowerCase();
+        return searchTerms.every((term: string) =>
+          cellValueString.includes(term)
+        );
+        // return (
+        //   cellValue &&
+        //   cellValue.toString().toLowerCase().includes(filterValue.toLowerCase())
+        // );
+      });
+    }
   });
 
-  // TODO: apply loading in table so used, empty tablerow
   return (
-    <>
-      <DataTableToolbar
-        table={table}
-        columnToSearch={columnToSearch}
-        columnName={columnName}
-        actions={children}
-      />
+    <div className="w-full">
+      <div className="flex items-center gap-x-2 pb-4">
+        {columnsToSearch && columnsToSearch.length > 0 && (
+          <div className="relative w-full sm:max-w-lg">
+            <Input
+              placeholder={placeholderSearch || 'Buscar por...'}
+              disabled={table.getFilteredSelectedRowModel().rows.length > 0}
+              value={globalFilter}
+              onChange={(e) => setGlobalFilter(e.target.value)}
+              className=""
+            />
+            {globalFilter.length > 0 && (
+              <div
+                className="absolute right-3 top-2 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-card text-muted-foreground opacity-70 ring-offset-background transition-all hover:bg-background hover:opacity-100 focus:outline-none disabled:pointer-events-none"
+                onClick={() => setGlobalFilter('')}
+              >
+                <Cross2Icon className="h-5 w-5" />
+                <span className="sr-only">Clean Input</span>
+              </div>
+            )}
+          </div>
+        )}
+        <div className="ml-auto flex gap-2">
+          {handleDeleteSelected && (
+            <MenuSelection
+              table={table}
+              handleDeleteSelected={handleDeleteSelected}
+            />
+          )}
+
+          {children && children}
+        </div>
+      </div>
+
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -161,32 +171,32 @@ export function DataTable<TData, TValue>({
                 </TableRow>
               ))
             ) : (
-              <TableRow>
+              <TableRow className="hover:bg-transparent">
                 <TableCell
                   colSpan={columns.length}
-                  className="h-24 text-center text-muted-foreground/70"
+                  className="h-24 text-center"
                 >
-                  No hay registros.
+                  <div className="flex w-full flex-col items-center px-2 pt-4 text-muted-foreground">
+                    <Icons.check className="h-10 w-10" />
+                    <div className="flex flex-col items-center py-4">
+                      <div className="text-sm text-muted-foreground">
+                        No hay resultados.
+                      </div>
+                    </div>
+                  </div>
                 </TableCell>
               </TableRow>
             )}
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 pt-4">
-        <div className="flex-1 text-sm text-muted-foreground/60">
-          <span className="mr-1">
-            {table.getFilteredRowModel().rows.length} registro(s)
-          </span>
-          {range && (
-            <>
-              &#x2022; Mostrando registros del{' '}
-              {range.from?.toLocaleDateString()} al{' '}
-              {range.to?.toLocaleDateString()}
-            </>
-          )}
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} de{' '}
+          {table.getFilteredRowModel().rows.length} fila(s) seleccionada(s).
         </div>
-        <div className="space-x-2">
+        <div className="flex space-x-2">
           <Button
             variant="outline"
             size="sm"
@@ -205,6 +215,6 @@ export function DataTable<TData, TValue>({
           </Button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
