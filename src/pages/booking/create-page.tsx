538a62329fce8d368b3ef09156dfb78795685823
Markos Mk <markosmk@me.com>
2025-03-01 12:14:41 -0300

    feat: add create page for booking with form integration

---
+++ src/pages/booking/create-page.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { HeadingMain } from '@/components/heading-main';
+import { BookingEditForm } from './_components/booking-edit-form';
+
+export default function CreatePage() {
+  return (
+    <div className="relative mx-auto w-full max-w-2xl space-y-8 px-4 pb-4 md:px-6 md:pb-6">
+      <HeadingMain title="Crear Reserva" />
+      <Card>
+        <CardContent>
+          <BookingEditForm isFetching={false} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
