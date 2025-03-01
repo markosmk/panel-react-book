a62cf6c38e3165998b2a664ff6685871513ca4ab
Markos Mk <markosmk@me.com>
2025-03-01 12:03:21 -0300

    feat: enhance nav-link with submenu functionality and expand/collapse behavior

--- src/components/layout/nav-link.tsx
+++ src/components/layout/nav-link.tsx
@@ -1,6 +1,16 @@
+import { LinkProps, NavLink as NavigateLink } from 'react-router-dom';
+import { ChevronDownIcon } from 'lucide-react';
+
 import { IconSVG } from '@/components/icons';
 import { cn } from '@/lib/utils';
-import { LinkProps, NavLink as NavigateLink } from 'react-router-dom';
+
+type NavLinkProps = LinkProps & {
+  icon?: IconSVG;
+  children: React.ReactNode;
+  disabled?: boolean;
+  isExpanded?: boolean;
+  hasSubmenu?: boolean;
+};
 
 export function NavLink({
   children,
@@ -8,55 +18,90 @@ export function NavLink({
   icon: Icon,
   disabled,
   className,
+  isExpanded,
+  hasSubmenu,
   ...props
-}: LinkProps & {
-  icon: IconSVG;
-  children: React.ReactNode;
-  disabled?: boolean;
-}) {
+}: NavLinkProps) {
   return (
-    <NavigateLink
-      to={to}
+    <div
       className={cn(
         'group block rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background',
         disabled && 'pointer-events-none select-none opacity-50',
-        className
+        isExpanded && 'pointer-events-none select-none'
       )}
-      {...props}
     >
-      {({ isActive }) => {
-        return (
+      {!hasSubmenu ? (
+        <NavigateLink to={to} end {...props}>
+          {({ isActive }) => (
+            <div
+              className={cn(
+                'relative mb-1 flex cursor-pointer flex-col rounded-md bg-transparent px-3 py-2.5 text-base transition-transform duration-200 active:scale-[.97]',
+                isActive ? 'bg-gray-active' : 'text-muted-foreground',
+                disabled && 'pointer-events-none select-none opacity-50',
+                className
+              )}
+            >
+              <div className="absolute inset-0 scale-75 rounded-md bg-gray-active opacity-0 transition-transform duration-200 ease-in-out group-hover:animate-scale-in" />
+              <div className="z-[1] flex w-full items-center overflow-auto transition-colors duration-200 ease-in-out group-hover:text-accent-foreground">
+                {/* mark active */}
+                {isActive && (
+                  <div className="visible absolute left-0 h-6 w-1 animate-scale-in rounded-md bg-primary" />
+                )}
+                {Icon && (
+                  <div className="ml-[0.1rem] mr-3 flex size-5 flex-shrink-0 items-center justify-center">
+                    <Icon className="size-5" />
+                  </div>
+                )}
+                <span className="flex-grow truncate text-left capitalize">
+                  {children}
+                </span>
+              </div>
+            </div>
+          )}
+        </NavigateLink>
+      ) : (
+        <button
+          type="button"
+          role="button"
+          aria-label="Open submenu"
+          className="w-full"
+        >
           <div
             className={cn(
               'relative mb-1 flex cursor-pointer flex-col rounded-md bg-transparent px-3 py-2.5 text-base transition-transform duration-200 active:scale-[.97]',
-              // hover:bg-gray-active hover:text-accent-foreground
-              isActive && 'bg-gray-active',
-              !isActive && 'text-muted-foreground',
-              disabled && 'pointer-events-none select-none opacity-50'
+              isExpanded ? 'bg-transparent' : 'text-muted-foreground',
+              disabled && 'pointer-events-none select-none opacity-50',
+              className
             )}
           >
-            {/* <div className="absolute inset-0 scale-75 rounded-md bg-gray-active opacity-0 transition-transform duration-200 group-hover:scale-100 group-hover:opacity-100"></div> */}
-            <div className="absolute inset-0 scale-75 rounded-md bg-gray-active opacity-0 transition-transform duration-200 ease-in-out group-hover:animate-scale-in"></div>
-
-            <div
-              className={cn(
-                'z-[1] flex w-full items-center overflow-auto transition-colors duration-200 ease-in-out group-hover:text-accent-foreground'
-              )}
-            >
+            {!isExpanded && (
+              <div className="absolute inset-0 scale-75 rounded-md bg-gray-active opacity-0 transition-transform duration-200 ease-in-out group-hover:animate-scale-in" />
+            )}
+            <div className="z-[1] flex w-full items-center overflow-auto transition-colors duration-200 ease-in-out group-hover:text-accent-foreground">
               {/* mark active */}
-              {isActive && (
-                <div className="visible absolute left-0 h-6 w-1 animate-scale-in rounded-md bg-primary"></div>
+              {isExpanded && (
+                <div className="visible absolute left-0 h-6 w-1 animate-scale-in rounded-md bg-primary" />
               )}
-              <div className="ml-[0.1rem] mr-3 flex h-5 w-5 flex-shrink-0 items-center justify-center">
-                {Icon && <Icon className="h-5 w-5" />}
-              </div>
+              {Icon && (
+                <div className="ml-[0.1rem] mr-3 flex size-5 flex-shrink-0 items-center justify-center">
+                  <Icon className="size-5" />
+                </div>
+              )}
               <span className="flex-grow truncate text-left capitalize">
                 {children}
               </span>
+              <div
+                className={cn(
+                  'absolute right-3 transition-transform duration-300',
+                  isExpanded ? 'rotate-0' : '-rotate-90'
+                )}
+              >
+                <ChevronDownIcon className="h-5 w-5" />
+              </div>
             </div>
           </div>
-        );
-      }}
-    </NavigateLink>
+        </button>
+      )}
+    </div>
   );
 }
--- src/components/layout/sidenav.tsx
+++ src/components/layout/sidenav.tsx
@@ -1,25 +1,96 @@
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+import type { NavigationItem } from '@/constants/navigation';
+import type { Role } from '@/types/user.types';
+
 import { adminNavigation } from '@/constants/navigation';
+import { useAuthStore } from '@/stores/use-auth-store';
 
 import { NavLink } from './nav-link';
 
-import { useAuthStore } from '@/stores/use-auth-store';
-import { Role } from '@/types/user.types';
-
 export function Sidenav() {
   const { user } = useAuthStore();
+  const [expandedMenu, setExpandedMenu] = React.useState<string | null>(null);
+  const location = useLocation();
+
+  React.useEffect(() => {
+    const currentPath = location.pathname;
+    const matchingMenu = adminNavigation.find(({ submenu }) => {
+      if (submenu) {
+        return submenu.some((subItem) => subItem.href === currentPath);
+      }
+      return false;
+    });
+
+    if (matchingMenu) {
+      setExpandedMenu(matchingMenu.name);
+    } else {
+      setExpandedMenu(null);
+    }
+  }, [location]);
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLDivElement>,
+    hasSubmenu: boolean,
+    name: string
+  ) => {
+    if (hasSubmenu) {
+      event.preventDefault();
+      event.stopPropagation();
+      setExpandedMenu(expandedMenu === name ? null : name);
+    } else {
+      setExpandedMenu(null);
+    }
+  };
+
+  const renderSubmenu = (submenu: NavigationItem[]) => (
+    <ul className="pl-4">
+      {submenu.map((subItem) => (
+        <li key={subItem.name}>
+          <NavLink
+            to={subItem.href}
+            disabled={subItem.disabled}
+            className="mr-4 text-sm"
+          >
+            {subItem.name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <nav className="nav-content scroller relative mt-3 w-full px-4 sm:mt-6 sm:py-2">
       <div className="block">
         <ul>
-          {adminNavigation.map(({ icon: Icon, ...item }) => {
+          {adminNavigation.map(({ icon: Icon, submenu, ...item }) => {
             if (item.role && user && !item.role.includes(user.role as Role))
               return null;
 
+            const hasSubmenu = !!submenu;
+            const isExpanded = expandedMenu === item.name;
+
             return (
-              <li key={item.name}>
-                <NavLink to={item.href} icon={Icon} disabled={item.disabled}>
-                  {item.name}
-                </NavLink>
+              <li
+                key={item.name}
+                className={isExpanded ? 'rounded-lg bg-background pb-2' : ''}
+              >
+                <div
+                  onClick={(e) => handleClick(e, hasSubmenu, item.name)}
+                  className="cursor-pointer"
+                >
+                  <NavLink
+                    to={hasSubmenu ? '#' : item.href}
+                    icon={Icon}
+                    disabled={item.disabled}
+                    isExpanded={isExpanded}
+                    hasSubmenu={hasSubmenu}
+                    onClick={(e) => hasSubmenu && e.stopPropagation()}
+                  >
+                    {item.name}
+                  </NavLink>
+                </div>
+                {hasSubmenu && isExpanded && renderSubmenu(submenu)}
               </li>
             );
           })}
