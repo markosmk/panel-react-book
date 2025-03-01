afee3f893403a7c9508929446bb8c1a00f71c54c
Markos Mk <markosmk@me.com>
2025-03-01 11:54:10 -0300

    feat: update button styles and enhance select component with loading state

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import {
-  CaretSortIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronUpIcon
-} from '@radix-ui/react-icons';
 import * as SelectPrimitive from '@radix-ui/react-select';
 
 import { cn } from '@/lib/utils';
+import { Icons } from '../icons';
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronsUpDown,
+  ChevronUpIcon
+} from 'lucide-react';
 
 const Select = SelectPrimitive.Root;
 
@@ -17,19 +18,27 @@ const SelectValue = SelectPrimitive.Value;
 
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> & {
+    isLoading?: boolean;
+  }
+>(({ className, children, isLoading = false, ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-primary/70 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-4 py-2 text-base shadow-sm ring-offset-background hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary/70 disabled:cursor-not-allowed disabled:opacity-50 data-[placeholder]:text-muted-foreground/70 [&>span]:line-clamp-1',
       className
     )}
+    disabled={isLoading || props.disabled}
     {...props}
   >
     {children}
+
     <SelectPrimitive.Icon asChild>
-      <CaretSortIcon className="h-4 w-4 opacity-50" />
+      {isLoading ? (
+        <Icons.spinner className="size-6" />
+      ) : (
+        <ChevronsUpDown className="size-4 opacity-50" />
+      )}
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
@@ -128,7 +137,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <CheckIcon className="h-4 w-4" />
+        <CheckIcon className="size-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
