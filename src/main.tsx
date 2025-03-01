82a57daf6e2a3fbfc817cd420ab16c8e7a782f28
Markos Mk <markosmk@me.com>
2025-03-01 11:47:41 -0300

    feat: add strict mode toggle using VITE_USE_STRICT_MODE environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  import.meta.env.VITE_USE_STRICT_MODE === 'true' ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
     <App />
-  </React.StrictMode>
+  )
 );
